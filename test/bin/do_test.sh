05666fe38bc23c65a03b74e1280494c85ee5beae
steveri <steveri@steveri.com>
2021-06-22 16:01:34 -0700

    build a dumb do_test thingy

---
+++ test/bin/do_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$1" == "--help" ]; then
+    echo "Examples:"
+    echo "  $0 ADD |& tee ADD.log ; # Results to ADD.results"
+    echo "  $0 SUB |& tee SUB.log ; # Results to SUB.results"
+    echo "  $0 MUL |& tee MUL.log ; # Results to MUL.results"
+    exit
+fi
+make clean
+make build TEST=$1
+make run TEST=$1 | tee $1.log
